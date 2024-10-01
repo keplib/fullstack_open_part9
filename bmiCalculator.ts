d9a0ec81c2a17adbbf0756b788555b668d2395f7
keplib <keplib@gmail.com>
2024-10-01 13:51:09 +0200

    day9 factories

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -5,13 +5,16 @@ const calculateBMI = (height: number, weight: number): string => {
 
   if (bmi <= 18.4) {
     return 'Underweight';
-  } else if (bmi >= 18.5 || bmi <= 24.9) {
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
     return 'Normal weight';
-  } else if (bmi >= 25.0 || bmi <= 39.9) {
+  } else if (bmi >= 25.0 && bmi <= 39.9) {
     return 'Overweight';
   } else {
     return 'Obese';
   }
 };
 
-console.log(calculateBMI(185, 86));
+const height: number = Number(process.argv[2]);
+const weight: number = Number(process.argv[3]);
+
+console.log(calculateBMI(height, weight));
