72840d50fe01e561901901d37a755e5f55b18af1
keplib <keplib@gmail.com>
2024-10-04 12:22:40 +0200

    exercise9.5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,16 +1,16 @@
-interface MultiplyValues {
-  value1: number;
-  value2: number;
+interface BMIValues {
+  height: number;
+  weight: number;
 }
 
-const parseArgs = (args: string[]): MultiplyValues => {
+const parseArgs = (args: string[]): BMIValues => {
   if (args.length < 4) throw new Error('Not enough arguments');
   if (args.length > 4) throw new Error('Too many arguments');
 
   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
     return {
-      value1: Number(args[2]),
-      value2: Number(args[3]),
+      height: Number(args[2]),
+      weight: Number(args[3]),
     };
   } else {
     throw new Error('Provided values were not numbers!');
@@ -18,31 +18,44 @@ const parseArgs = (args: string[]): MultiplyValues => {
 };
 
 const calculateBMI = (height: number, weight: number): string => {
+  if (height === 0 || weight === 0) {
+    throw new Error('Height or weight cannot be zero. Please provide a valid value.');
+  }
+
   const heightInMeters = height / 100;
 
   const bmi = weight / (heightInMeters * heightInMeters);
 
-  if (bmi <= 18.4) {
-    return 'Underweight';
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    return 'Normal weight';
-  } else if (bmi >= 25.0 && bmi <= 39.9) {
-    return 'Overweight';
+  const UNDERWEIGHT_THRESHOLD = 18.5;
+  const NORMAL_THRESHOLD = 25;
+  const OVERWEIGHT_THRESHOLD = 30;
+
+  let bmiCategory: string;
+
+  if (bmi <= UNDERWEIGHT_THRESHOLD) {
+    bmiCategory = 'Underweight';
+  } else if (bmi <= NORMAL_THRESHOLD) {
+    bmiCategory = 'Normal weight';
+  } else if (bmi <= OVERWEIGHT_THRESHOLD) {
+    bmiCategory = 'Overweight';
   } else {
-    return 'Obese';
+    bmiCategory = 'Obese';
   }
+
+  return bmiCategory;
 };
 
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
-
-try {
-  const { value1, value2 } = parseArgs(process.argv);
-  console.log(calculateBMI(value1, value2));
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArgs(process.argv);
+    console.log(calculateBMI(height, weight));
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happened.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
+
+export default calculateBMI;
--- index.ts
+++ index.ts
@@ -1,11 +1,38 @@
 import express from 'express';
+import calculateBMI from './bmiCalculator';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (_, res) => {
   res.send('Hello Full Stack!');
 });
 
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+
+  try {
+    const heightValue: number = parseFloat(height as string);
+    const weightValue: number = parseFloat(weight as string);
+
+    if (isNaN(heightValue) || isNaN(weightValue)) {
+      throw Error();
+    }
+
+    const bmi: string = calculateBMI(heightValue, weightValue);
+
+    res.json({
+      weight: weightValue,
+      height: heightValue,
+      bmi: bmi,
+    });
+  } catch (error) {
+    res.status(404).json({
+      error: 'malformatted parameters',
+    });
+  }
+});
 const PORT = 3003;
 
 app.listen(PORT, () => {
